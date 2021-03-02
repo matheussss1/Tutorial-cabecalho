980528aef05dfe7031d5d35e1e012a9a19364ac0
Matheus <matheusfelipe_contato@hotmail.com>
2021-03-01 21:40:13 -0300

    IM A GENIUS

--- js/menu-lateral.js
+++ js/menu-lateral.js
@@ -1,10 +1,22 @@
-    var menu = $('.menu-lateral');
+    // var menu = $('.menu-lateral');
 
-    $('.abre-menu').click(function(){
-        menu.is(':hidden')? menu.slideToggle('slow') : false;
-        $('.overlay').removeAttr('hidden');
+    // $('.abre-menu').click(function(){
+    //     menu.is(':hidden')? menu.slideToggle('slow') : false;
+    //     $('.overlay').removeAttr('hidden');
+    // })
+    // $('.fecha-menu').click(function(){
+    //     !menu.is(':hidden')? menu.slideToggle('slow') : false;
+    //     $('.overlay').attr('hidden', 'hidden');
+    // })
+
+    var menuLateral = document.querySelector('.menu-lateral')
+    var abreMenu = document.querySelector('.abre-menu')
+    var fechaMenu = document.querySelector('.fecha-menu')
+
+    abreMenu.addEventListener('click', function(){
+        menuLateral.classList.add('animacao-abre')
     })
-    $('.fecha-menu').click(function(){
-        !menu.is(':hidden')? menu.slideToggle('slow') : false;
-        $('.overlay').attr('hidden', 'hidden');
+    fechaMenu.addEventListener('click', function(){
+        menuLateral.classList.remove('animacao-abre')
+        menuLateral.classList.add('animacao-fecha')
     })
